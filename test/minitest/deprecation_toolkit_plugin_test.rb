2398f38acb62220fb79a6cd720f61d9cea26bc06
Jean Boussier <jean.boussier@gmail.com>
2020-04-28 16:40:28 +0200

    Update CI for newer rubies

--- test/minitest/deprecation_toolkit_plugin_test.rb
+++ test/minitest/deprecation_toolkit_plugin_test.rb
@@ -16,14 +16,12 @@ module Minitest
     end
 
     test ".plugin_deprecation_toolkit_init set the behavior to `Record` when `record_deprecations` options is true" do
-      begin
-        previous_behavior = DeprecationToolkit::Configuration.behavior
-        Minitest.plugin_deprecation_toolkit_init(record_deprecations: true)
+      previous_behavior = DeprecationToolkit::Configuration.behavior
+      Minitest.plugin_deprecation_toolkit_init(record_deprecations: true)
 
-        assert_equal(DeprecationToolkit::Behaviors::Record, DeprecationToolkit::Configuration.behavior)
-      ensure
-        DeprecationToolkit::Configuration.behavior = previous_behavior
-      end
+      assert_equal(DeprecationToolkit::Behaviors::Record, DeprecationToolkit::Configuration.behavior)
+    ensure
+      DeprecationToolkit::Configuration.behavior = previous_behavior
     end
 
     test ".plugin_deprecation_toolkit_init add `notify` behavior to the deprecations behavior list" do
@@ -62,19 +60,17 @@ module Minitest
     end
 
     test ".plugin_deprecation_toolkit_init doesn't init plugin when outside bundler context" do
-      begin
-        notify_behavior = ActiveSupport::Deprecation::DEFAULT_BEHAVIORS[:notify]
-        old_bundle_gemfile = ENV['BUNDLE_GEMFILE']
-        ENV.delete('BUNDLE_GEMFILE')
+      notify_behavior = ActiveSupport::Deprecation::DEFAULT_BEHAVIORS[:notify]
+      old_bundle_gemfile = ENV['BUNDLE_GEMFILE']
+      ENV.delete('BUNDLE_GEMFILE')
 
-        ActiveSupport::Deprecation.behavior.delete(notify_behavior)
-        Minitest.plugin_deprecation_toolkit_init({})
+      ActiveSupport::Deprecation.behavior.delete(notify_behavior)
+      Minitest.plugin_deprecation_toolkit_init({})
 
-        refute_includes(ActiveSupport::Deprecation.behavior, notify_behavior)
-      ensure
-        ENV['BUNDLE_GEMFILE'] = old_bundle_gemfile
-        ActiveSupport::Deprecation.behavior << notify_behavior
-      end
+      refute_includes(ActiveSupport::Deprecation.behavior, notify_behavior)
+    ensure
+      ENV['BUNDLE_GEMFILE'] = old_bundle_gemfile
+      ActiveSupport::Deprecation.behavior << notify_behavior
     end
   end
 end
