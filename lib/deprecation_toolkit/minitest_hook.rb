7e50593b86429c6e63cc09fea117b79251bafebf
Aiden Storey <git@storey.dev>
2026-05-07 11:35:05 -0400

    Update to support minitest 6 plugin loading

--- lib/deprecation_toolkit/minitest_hook.rb
+++ lib/deprecation_toolkit/minitest_hook.rb
@@ -2,6 +2,10 @@
 
 require "minitest"
 
+if Minitest.respond_to?(:load) && !Minitest.extensions.include?("deprecation_toolkit")
+  Minitest.load(:deprecation_toolkit)
+end
+
 module DeprecationToolkit
   module Minitest
     def trigger_deprecation_toolkit_behavior
